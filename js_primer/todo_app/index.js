6c352bc00d6a01b9d7aea2e7c2fec8c6410b5dfb
KoheiNishino <koheinishino.95@gmail.com>
2021-07-09 09:13:25 +0900

    応用編③ Todoアプリ

---
+++ js_primer/todo_app/index.js
@@ -0,0 +1,11 @@
+import { App } from "./src/App.js";
+
+const app = new App();
+// ページのロードが完了したときのイベント
+window.addEventListener("load", () => {
+    app.mount();
+});
+// ページがアンロードされたときのイベント
+window.addEventListener("unload", () => {
+    app.unmount();
+});
